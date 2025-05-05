94b5d8b9d3726610682aa02de052f2e6cd79fb70
Sean Murphy <sean@iamseanmurphy.com>
2025-05-05 13:21:28 +0000

    12.7.3

--- review-widgets-for-tripadvisor/uninstall.php
+++ review-widgets-for-tripadvisor/uninstall.php
@@ -3,6 +3,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 die;
 }
 require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_tripadvisor = new TrustindexPlugin_tripadvisor("tripadvisor", __FILE__, "12.7.2", "WP Tripadvisor Review Widgets", "Tripadvisor");
+$trustindex_pm_tripadvisor = new TrustindexPlugin_tripadvisor("tripadvisor", __FILE__, "12.7.3", "WP Tripadvisor Review Widgets", "Tripadvisor");
 $trustindex_pm_tripadvisor->uninstall();
 ?>
